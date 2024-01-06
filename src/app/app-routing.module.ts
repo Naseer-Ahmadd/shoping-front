546ec735f9c069e94d5e049fcb0ee8d03b1b2b5b
Naseer Ahmad <naseerrather58@gmail.com>
2024-01-06 15:19:09 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: '',
+    loadChildren: ()=> import('./home/home.module').then((m)=>{ return m.HomeModule})
+  },
+  {
+    path: 'products',
+    loadChildren: ()=> import('./product-listing/product-listing.module').then(m=>m.ProductListingModule)
+  }
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {scrollPositionRestoration: 'top',})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
